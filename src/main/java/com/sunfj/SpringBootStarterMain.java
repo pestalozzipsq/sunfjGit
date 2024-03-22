8295334fec03a0d463a3cbdb7ee29165a5423dbb
pestalozzipsq <290468985@qq.com>
2024-03-22 15:09:40 +0800

    测试1

--- src/main/java/com/sunfj/SpringBootStarterMain.java
+++ src/main/java/com/sunfj/SpringBootStarterMain.java
@@ -1,12 +1,18 @@
 package com.sunfj;
 
+import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
 
 @SpringBootApplication
 public class SpringBootStarterMain {
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootStarterMain.class,args);
+        ConfigurableApplicationContext run = SpringApplication.run(SpringBootStarterMain.class, args);
+        Environment env = run.getEnvironment();
+        String xmxValue = env.getProperty("java.vm.options");
+        System.out.println("Xmx value: " + xmxValue);
     }
 }
--- src/main/java/com/sunfj/controller/TestController.java
+++ src/main/java/com/sunfj/controller/TestController.java
@@ -1,13 +1,16 @@
 package com.sunfj.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/a")
 public class TestController {
+
+
     @RequestMapping("/test")
     public String test(){
-        return "a-test";
+        return "hello world";
     }
 }
